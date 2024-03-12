f8e1239d0b1b88cd88d5c2ab90e4bbe6a3856206
Sean Kelly <sean_emailer@protonmail.com>
2024-03-12 08:32:48 +0000

    further configuration

--- src/tests/angular-example.spec.ts
+++ src/tests/angular-example.spec.ts
@@ -1,8 +1,9 @@
-import { TestBed, ComponentFixture, waitForAsync } from '@angular/core/testing';
+import { TestBed, ComponentFixture, waitForAsync, async, tick } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import { UiCollectionviewSeanComponent } from '../app/ui-collectionview-demo/ui-collectionview-sean.component';
 import 'zone.js';
 import 'zone.js/testing';
+import { Device } from '@nativescript/core';
 
 // Initialize the test environment
 TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
@@ -11,15 +12,41 @@ describe('UiCollectionviewSeanComponent', () => {
   let component: UiCollectionviewSeanComponent;
   let fixture: ComponentFixture<UiCollectionviewSeanComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({ declarations: [UiCollectionviewSeanComponent] }); // Compile the component template
-    fixture = TestBed.createComponent(UiCollectionviewSeanComponent);
-    component = fixture.componentInstance;
-  });
+  beforeEach(waitForAsync(async () => {
+    await TestBed.configureTestingModule({ 
+        providers: [
+            UiCollectionviewSeanComponent
+        ] 
+    }).compileComponents(); // Compile the component template
+    }));
 
-  it('should create', () => {
+    // describe("A suite", function() {
+    //     it("contains spec with an expectation", function() {
+    //       expect(true).toBe(true);
+    //     });
+    //   });
+      
+    //   describe("iOS Device", function() {
+    //     console.log('sdk version', Device.sdkVersion);
+    //     it("should use > 14 SDK", function() {
+    //       expect(Device.sdkVersion).toBeGreaterThan(14);
+    //     });
+    //   });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UiCollectionviewSeanComponent);
+        component = fixture.componentInstance;
+    });
+
+it('should create', () => {
+      fixture = TestBed.createComponent(UiCollectionviewSeanComponent);
+      fixture.detectChanges();
+      tick(2000);
+      fixture.detectChanges();
+
+      component = fixture.debugElement.componentInstance;
     // Check if the component is created successfully
-    expect(component).toBeDefined();
+    expect(component).toBeTruthy();
   });
 
   // Add more test cases as needed
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require("@nativescript/webpack");
+const { merge } = require("webpack-merge"); // Import the merge function
 
 module.exports = (env) => {
 	webpack.init(env);
@@ -6,22 +7,21 @@ module.exports = (env) => {
 	// Learn how to customize:
 	// https://docs.nativescript.org/webpack
 
-  // TODO: work out why this FAILS
-  // if (env.unitTesting == true) {
-  //   webpack.chainWebpack((config) => {
-  //     config.plugin('AngularWebpackPlugin').tap((args) => {
-  //       args[0].tsconfig = './tsconfig.spec.json'
-  //       return args
-  //     })
-  //     // keep the test runner open
-  //     config.plugin('DefinePlugin').tap((args) => {
-  //       args[0] = merge(args[0], {
-  //         __TEST_RUNNER_STAY_OPEN__: true,
-  //       })
-  //       return args
-  //     })
-  //   })
-  // }
+  if (env.unitTesting == true) {
+    webpack.chainWebpack((config) => {
+      config.plugin('AngularWebpackPlugin').tap((args) => {
+        args[0].tsconfig = './tsconfig.spec.json'
+        return args
+      })
+      // keep the test runner open
+      config.plugin('DefinePlugin').tap((args) => {
+        args[0] = merge(args[0], {
+          __TEST_RUNNER_STAY_OPEN__: true,
+        })
+        return args
+      })
+    })
+  }
 
 	return webpack.resolveConfig();
 };
