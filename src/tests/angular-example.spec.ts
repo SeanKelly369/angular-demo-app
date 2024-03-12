fbfd72e66a3016378cab704d5a9ad54b13ae5f20
Sean Kelly <sean_emailer@protonmail.com>
2024-03-12 00:30:20 +0000

    The joys of software tests that don't do anything

--- src/tests/angular-example.spec.ts
+++ src/tests/angular-example.spec.ts
@@ -1,23 +1,27 @@
 import { TestBed } from '@angular/core/testing'
 import { UiCollectionviewSeanComponent } from '../app/ui-collectionview-demo/ui-collectionview-sean.component'
 import { Page } from '@nativescript/core'
-import 'zone.js/dist/zone-testing'; // Uncommented this line
+import 'zone.js';
 
 describe('search.component', () => {
   beforeEach(async () => {
+    console.log('hitting beforeEach')
     await TestBed.configureTestingModule({
       declarations: [UiCollectionviewSeanComponent],
       providers: [Page],
     }).compileComponents()
+    console.log('hitting after compileComponents')
   })
 
   it('should fail', async () => {
+    console.log('hitting should fail')
     const fixture = TestBed.createComponent(UiCollectionviewSeanComponent)
     const component = fixture.componentInstance as UiCollectionviewSeanComponent
-    expect(component.addPlayers(10)).toBeTruthy()
+    expect(component.addPlayers(10)).not.toBeTruthy();
   })
 
   it('should succeed', async () => {
+    console.log('hitting should succeed')
     const fixture = TestBed.createComponent(UiCollectionviewSeanComponent)
     const component = fixture.componentInstance as UiCollectionviewSeanComponent
     expect(component.addPlayers(10)).toBeTruthy()
