7b06a55ffb88dc26278d0adca0e4ccc320886a34
Sean Kelly <sean_emailer@protonmail.com>
2024-03-18 20:01:22 +0000

    Unit testing working

--- src/tests/angular-example.spec.ts
+++ src/tests/angular-example.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { CollectionViewModule } from "@nativescript-community/ui-collectionview/angular";
 import { Page } from "@nativescript/core";
 import { UiCollectionviewSeanComponent } from "~/app/ui-collectionview-demo/ui-collectionview-sean.component";
 describe("search.component", () => {
@@ -8,20 +9,21 @@ describe("search.component", () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [UiCollectionviewSeanComponent],
-            providers: [
-                Page
-            ]
+            providers: [Page],
+            imports: [CollectionViewModule]
         }).compileComponents();
     });
 
     beforeEach(() => {
         fixture = TestBed.createComponent(UiCollectionviewSeanComponent);
-        component = fixture.componentInstance;
+        component = <UiCollectionviewSeanComponent>fixture.componentInstance;
     });
 
-    it("should fail", async () => {
-        fixture = TestBed.createComponent(UiCollectionviewSeanComponent);
-        component = fixture.componentInstance as UiCollectionviewSeanComponent;
+    it("should be true", async () => {
         expect(component.setTest()).toBeTrue();
+    });
+
+    it("should be false", async () => {
+        expect(component.setTest()).not.toBeFalse();
     });
 });
