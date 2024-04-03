43e0c7466c00f7347f753ee7fa2835f4dad3755d
Sean Kelly <skelly@munsterbovine.ie>
2024-04-03 22:31:38 +0100

    ui components demo code

--- src/app/ui-components/ui-components.module.ts
+++ src/app/ui-components/ui-components.module.ts
@@ -1,15 +1,15 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule, NativeScriptRouterModule } from '@nativescript/angular';
 import { CollectionViewModule } from '@nativescript-community/ui-collectionview/angular';
-import { UiCollectionviewSeanComponent } from './ui-collectionview-sean.component';
+import { UiComponentsComponent } from '../ui-components/ui-components.component';
 
 @NgModule({
   imports: [NativeScriptCommonModule, CollectionViewModule, NativeScriptRouterModule.forChild(
     [
-      { path: '', component: UiCollectionviewSeanComponent }
+      { path: '', component: UiComponentsComponent }
     ]
   )],
-  declarations: [UiCollectionviewSeanComponent],
+  declarations: [UiComponentsComponent],
   schemas: [ NO_ERRORS_SCHEMA]
 })
 export class UiCollectionviewSeanModule {}
